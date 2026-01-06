25f3d1c20bc0efbaac545482275d5707d22298f7
Gian Maria Troiani <gianmariatroiani@gmail.com>
2026-01-06 07:46:49 -0500

    style(reader): Reduce Assistant panel width by 1/6 (512px → 432px)

--- web/src/components/reader/AssistantPanel.jsx
+++ web/src/components/reader/AssistantPanel.jsx
@@ -77,7 +77,7 @@ export default function AssistantPanel({
   }
 
   return (
-    <div className="w-[32rem] border-l border-gray-300 bg-gray-200 flex flex-col">
+    <div className="w-[27rem] border-l border-gray-300 bg-gray-200 flex flex-col">
       {/* Header with tabs */}
       <div className="flex items-center justify-between border-b border-gray-300 px-2 bg-gray-100">
         <div className="flex">
